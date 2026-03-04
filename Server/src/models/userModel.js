0001b8c4dde7af9d78aa683c0d365def1d99178c
Piyush1532 <piyushadav503@gmail.com>
2026-03-04 14:49:55 +0530

    user model

---
+++ Server/src/models/userModel.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose"
+
+const userSchema=new mongoose.Schema({
+    username:{type:String,required:true,unique:[true,"Username already taken"]},
+    email:{
+        type:String,
+        required:true,
+        unique:[true,"Account already exist with this email address"]
+    },
+    password:{type:String,required:true}
+},{timestamps:true})
+
+const userModel=mongoose.model("User",userSchema)
+
+export default userModel
