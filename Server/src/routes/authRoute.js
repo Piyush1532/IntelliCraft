e4f2d076c42c6293abd6ede3872628f04d13c8e8
Piyush1532 <piyushadav503@gmail.com>
2026-03-06 13:59:42 +0530

    added new route

--- Server/src/routes/authRoute.js
+++ Server/src/routes/authRoute.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { loginUser, registerUser } from "../controllers/authController.js"
+import { loginUser, logoutUser, registerUser } from "../controllers/authController.js"
 
 const authRouter=express.Router()
 
@@ -8,5 +8,8 @@ authRouter.post("/register",registerUser)
 
 // login a user through email and password
 authRouter.post("/login",loginUser)
+
+// clear token from user cookie and add the token in the blacklist
+authRouter.get("/logout",logoutUser)
 
 export default authRouter
